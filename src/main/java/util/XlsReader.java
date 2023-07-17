5a66ebff1546709f8f4224576568bb453e26c9b3
Anton Kartashov <anton-kartashov@list.ru>
2023-07-17 23:30:06 +0300

    Изменил название класса

--- src/main/java/util/XlsReader.java
+++ src/main/java/util/XlsReader.java
@@ -13,9 +13,9 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class ExcelReader {
+public class XlsReader {
 
-    private ExcelReader(){
+    private XlsReader(){
 
     }
 
